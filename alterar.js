8a8b91ea1ba9278ae28497703d06078f39a2a183
G <geovana.pereira.costa@escola.pr.gov.br>
2026-09-04 14:33:47 +0000

    ...

--- alterar.js
+++ alterar.js
@@ -8,3 +8,12 @@ pessoa.telefone = "123456789";
 console.log(pessoa.telefone);
 
 console.log(pessoa.nome);
+
+pessoa.nome = "Luma Silva";
+
+console.log(pessoa.nome);
+
+const Novapessoa = {
+    nome: "Pedro";
+};
+
